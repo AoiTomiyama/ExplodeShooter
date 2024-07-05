67c7a0298a415aa5de1fe0a83370e4cb74261559
AoiTomiyama <TadoO.o2209@gmail.com>
2024-07-06 02:23:10 +0900

    自機が画面外に行けないようにRaycastを追加。

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -18,6 +18,25 @@ public class PlayerMove : MonoBehaviour
     {
         _h = Input.GetAxisRaw("Horizontal");
         _v = Input.GetAxisRaw("Vertical");
-        transform.Translate(new Vector2(_h, _v) * _moveSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        if (_h != 0 || _v != 0)
+        {
+            float lineLengthVertical = 0.5f;
+            float lineLengthHorizontal = 1f;
+            var hitLeft = Physics2D.Linecast(transform.position, transform.position + Vector3.left * lineLengthHorizontal);
+            var hitRight = Physics2D.Linecast(transform.position, transform.position + Vector3.right * lineLengthHorizontal);
+            var hitUp = Physics2D.Linecast(transform.position, transform.position + Vector3.up * lineLengthVertical);
+            var hitDown = Physics2D.Linecast(transform.position, transform.position + Vector3.down * lineLengthVertical);
+            Debug.DrawLine(this.transform.position + Vector3.left * lineLengthHorizontal, this.transform.position + Vector3.right * lineLengthHorizontal);
+            Debug.DrawLine(this.transform.position + Vector3.up * lineLengthVertical, this.transform.position + Vector3.down * lineLengthVertical);
+            if (hitLeft && _h < 0) _h = 0;
+            if (hitRight && _h > 0) _h = 0;
+            if (hitDown && _v < 0) _v = 0;
+            if (hitUp && _v > 0) _v = 0;
+            transform.Translate(new Vector2(_h, _v) * _moveSpeed * Time.deltaTime);
+        }
     }
 }
